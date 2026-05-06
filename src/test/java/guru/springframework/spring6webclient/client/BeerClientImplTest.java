c1147d95a3cda35394cd96fbb2c2ba75cc987eff
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-06 16:58:26 -0600

    Adding testUpdate

--- src/test/java/guru/springframework/spring6webclient/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6webclient/client/BeerClientImplTest.java
@@ -16,6 +16,26 @@ class BeerClientImplTest {
     @Autowired
     BeerClient client;
 
+
+
+    @Test
+    void testUpdate() {
+        final String NAME = "New Name";
+
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        client.listBeerDtos()
+                .next()
+                .doOnNext(beerDTO -> beerDTO.setBeerName(NAME))
+                .flatMap(dto -> client. updateBeer(dto))
+                .subscribe(byIdDto -> {
+                    System.out.println(byIdDto.toString());
+                    atomicBoolean.set(true);
+                });
+
+        await().untilTrue(atomicBoolean);
+    }
+
     @Test
     void testCreateBeer() {
 
