80622c7a880cdb40a24092dc00fcb37ad4a3944d
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-06 16:01:15 -0600

    Adding BeerClient and its implementation and fixing some bugs

---
+++ src/test/java/guru/springframework/spring6webclient/client/BeerClientImplTest.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring6webclient.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerClientImplTest {
+
+    @Autowired
+    BeerClient client;
+
+    @Test
+    void listBeer() {
+        client.listBeers().subscribe(response -> System.out.println(response));
+    }
+}
