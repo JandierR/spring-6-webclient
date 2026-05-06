3ad0e5cd4e061323c87fcafcffa1c3cfeb3c1ce1
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-06 16:51:15 -0600

    Adding createBeer

--- src/main/java/guru/springframework/spring6webclient/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6webclient/client/BeerClientImpl.java
@@ -18,7 +18,19 @@ public class BeerClientImpl implements BeerClient {
 
     public BeerClientImpl(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.build();
-        ;
+    }
+
+    @Override
+    public Mono<BeerDTO> createBeer(BeerDTO beerDTO) {
+        return webClient.post()
+                .uri(BEER_PATH)
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(voidResponseEntity -> Mono.just(voidResponseEntity
+                        .getHeaders().get("Location").get(0)))
+                .map(path -> path.split("/")[path.split("/").length - 1])
+                .flatMap(this::getBeerById);
     }
 
     @Override
